af698cfa64bf145f955d780841304ee9711542c6
miciny <15201114041@163.com>
2018-07-16 16:15:24 +0800

    fix bugs

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,14 +9,20 @@ Page({
       goodsAll: '商品总金额:', 
       discountsAll: '优惠总金额:',
       freightAll: '总\xa0\xa0\xa0运\xa0\xa0\xa0费:',
-      aGoodsAll: '个人总金额:',
+      aGoodsAll: '\xa0的总金额:',
+      personNo: '总\xa0\xa0\xa0人\xa0\xa0\xa0数:',
     }, 
+
+    index: 0,
+    array: [2, 3, 4, 5, 6, 7, 8],
+    arrayA: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+    all: {},
 
     inputData_goodsAll: '',
     inputData_discountsAll: '',
     inputData_freightAll: '',
-    inputData_aGoodsAll: '', 
-    result: '请输入金额',  
+    result: '',
+    resultFinal: '',  
 
     focus: false,
     userInfo: {},
@@ -91,44 +97,106 @@ Page({
     })
   },
 
+  //选择人数
+  bindPickerChange: function (e) {
+    var oldIndex = this.data.index;
+    this.setData({
+      index: e.detail.value,
+      result: "",
+      resultFinal: "",
+    })
+
+    //重新选择人数，现在处理数据
+    if (oldIndex>this.data.index){
+        var oldAll = this.data.all;
+        var newArrayA = this.data.arrayA.slice(0, this.data.array[e.detail.value]);
+        for (var key in oldAll){
+          if (!this.isInArray3(newArrayA, key)){
+            delete oldAll[key];
+          }
+        }
+    }
+  },
+
+  isInArray3: function(arr, value){
+    if(arr.indexOf&&typeof (arr.indexOf) == 'function') {
+      var index = arr.indexOf(value);
+      if (index >= 0) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  inputB: function (e) {
+    var all = this.data.all;
+    var iname = e.target.dataset.iname;
+    all[iname] = e.detail.value;
+    this.setData({
+      all: all
+    });
+  },
+
   //计算显示
   doCalculate: function() {
-    var aa = (this.data.inputData_goodsAll);
-    var bb = (this.data.inputData_discountsAll);
-    var cc = (this.data.inputData_freightAll);
-    var dd = (this.data.inputData_aGoodsAll);
+    var a = parseFloat(this.data.inputData_goodsAll);
+    var b = parseFloat(this.data.inputData_discountsAll);
+    var c = parseFloat(this.data.inputData_freightAll);
 
-    if (aa == ""){
-      aa = "0"
-    }
-    if (bb == ""){
-      bb = "0"
-    }
-    if (cc == "") {
-      cc = "0"
-    }
-    if (dd == "") {
-      dd = "0"
-    }
-
-    var a = parseFloat(aa);
-    var b = parseFloat(bb);
-    var c = parseFloat(cc);
-    var d = parseFloat(dd);
+    if (isNaN(a)) { a = 0 };
+    if (isNaN(b)) { b = 0 };
+    if (isNaN(c)) { c = 0 };
 
     var str = "";
-    var ss = a - b + c
+    var strFinal = '';
+    var ss = a - b + c;
 
     if (a <= 0 || ss < 0 || b > a){
-      str = "数据有误"
+      wx.showModal({
+        title: '提示',
+        content: '数据有误',
+        showCancel: false
+      })  
     } else {
-      var ee = d - d / a * b + d / a * c
-      ee = ee.toFixed(2);
-      str = "实付：￥" + ss + ",\xa0\xa0\xa0\xa0\xa0个人应付：￥" + ee
+      ss = ss.toFixed(2);
+      str = "实\xa0\xa0\xa0付:￥" + ss
+      var totalPay = 0;
+      var totalGoods = 0;
+
+      for (var key in this.data.all) {
+        var d = parseFloat(this.data.all[key]);
+        if (isNaN(d)) { d = 0 };
+
+        var f = d / a * b;
+        var g = d / a * c;
+        var ee = d - f + g;
+
+        totalPay += ee;
+        totalGoods += d;
+        f = f.toFixed(2);
+        g = g.toFixed(2);
+        ee = ee.toFixed(2);
+
+        str += "\n" + key + "\xa0应付:￥" + ee + "\n\xa0\xa0分摊优惠:￥" + f + ",分摊运费:￥" + g
+      }
+
+      if (totalGoods != a){
+        wx.showModal({
+          title: '提示',
+          content: '个人金额总计:￥' + totalGoods + '与订单总计:￥' + a + '\n金额不一致',
+          showCancel: false
+        }) 
+        str = ""
+        strFinal = ""
+      }else{
+        totalPay = totalPay.toFixed(2);
+        strFinal += "应付总计:\xa0\xa0￥" + totalPay;
+      }
     }
 
     this.setData({
       result: str,
+      resultFinal: strFinal,
     })
   }
 })
