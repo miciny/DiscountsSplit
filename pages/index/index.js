8d4a9e904380e40afa6da6f04926988be6c5ecb0
miciny <15201114041@163.com>
2018-08-21 10:40:25 +0800

    加入新功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,16 +10,18 @@ Page({
       goodsAll: '商品总金额:', 
       discountsAll: '优惠总金额:',
       freightAll: '总\xa0\xa0\xa0运\xa0\xa0\xa0费:',
-      aGoodsAll: '\xa0的总金额:',
+      aGoodsAll: '(点击可输入)',
       personNo: '总\xa0\xa0\xa0人\xa0\xa0\xa0数:',
     }, 
 
-    listData: [], //结果展示数据
+    listData: [], //结果展示数据，【“A”：“22.20”】
     tabelShowFlag: false, //结果展示的table是否显示
     personNm: 0, //选择的人数的底标
     array: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], //选择的人数
     arrayA: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'], //选择的人数对应的字母
     focus: false, //第一个框是否聚焦
+
+    resetNull: "",
 
     userInfo: {}, //用户信息
     hasUserInfo: false,
@@ -95,6 +97,8 @@ Page({
   getPrice: function (price) {
     price = parseFloat(price)
     if (isNaN(price)) { price = 0 };
+    price = price.toFixed(2);
+    price = parseFloat(price)
     return price
   },
 
@@ -108,13 +112,23 @@ Page({
 
     //金额大于0，实付大于0，优惠小于商品金额
     if (a <= 0 || ss < 0 || b > a){
+      var str = ""
+      if (a <= 0){
+        str = "数据有误: 请输入正确的商品总金额"
+      } else if (b > a) {
+        str = "数据有误: 优惠金额大于商品总金额"
+      } else if (ss < 0) {
+        str = "数据有误: 实付金额小于0元"
+      }
+
       wx.showModal({
         title: '提示',
-        content: '数据有误',
+        content: str,
         showCancel: false
       })  
     } else {
       var listDataTem = [];
+      var nameListData = new Array();; //名字和字母的对应关系
       var totalGoods = 0; //记录每个人的实付金额（四舍五入之后的）
       var totalFreight = 0; //记录每个人的运费金额（四舍五入之后的）
       var totalDiscounts = 0; //记录每个人的优惠金额（四舍五入之后的）
@@ -146,7 +160,21 @@ Page({
             "pay": ee 
             }
           listDataTem.push(sPay)
+        }else{  //记录名字
+          if(key.indexOf("name_") != -1){
+            var name = e.detail.value[key]; //名字
+            var nameA = key.slice(5);
+            if(name == ""){name = nameA};  //如果为空
+            nameListData[nameA] = name;
+          }
         }
+      }
+
+      //处理名字问题
+      for (var item in listDataTem){
+        var nameA = listDataTem[item].name;
+        var name = nameListData[nameA];
+        listDataTem[item].name = name;
       }
 
       //各个人的金额总数等于商品金额
@@ -172,6 +200,14 @@ Page({
         tabelShowFlag: flag
       })
     }
+  },
+
+  //重置
+  resetData: function (e) {
+    this.setData({
+      personNm: 0,
+      resetNull: "",
+    })
   },
 
   //分享app
